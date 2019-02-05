072cce3af3af47f4014d77d1124ecb4a328d8268
JGomez2542 <jason.gomez@mobileappscompany.com>
2019-02-05 14:02:04 -0500

    Added WorkManager and PendingIntent actions to notification

--- app/src/main/java/com/example/jobschedulerex/NotificationWorker.kt
+++ app/src/main/java/com/example/jobschedulerex/NotificationWorker.kt
@@ -32,18 +32,43 @@ class NotificationWorker(private val context: Context, workerParameters: WorkerP
     @SuppressLint("NewApi")
     override fun doWork(): Result {
         val data = inputData.getString(WORKER_RANDOM_TOAST)
-        //Upon clicking the notification, we open up the browser.
-        val intent = Intent(Intent.ACTION_VIEW)
-        intent.data = Uri.parse(URL)
-        val browserPendingIntent = PendingIntent.getActivity(context, PENDING_INTENT_ID, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        //Tapping on the notification itself will direct the user to the application's main activity
+        val launchIntent = context.packageManager.getLaunchIntentForPackage(context.packageName)
+        val launchPendingIntent = PendingIntent.getActivity(context, LAUNCHER_PENDING_INTENT_ID, launchIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        //Tapping on the WorkManager action will direct the user to the WorkManager web page
+        val workManagerIntent = Intent(Intent.ACTION_VIEW)
+        workManagerIntent.data = Uri.parse(WORK_MANAGER_URL)
+        val workManagerPendingIntent = PendingIntent.getActivity(context, WORK_MANAGER_PENDING_INTENT_ID, workManagerIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        //Tapping on the PendingIntent action will direct the user to the PendingIntent web page
+        val pendingIntentIntent = Intent(Intent.ACTION_VIEW)
+        pendingIntentIntent.data = Uri.parse(PENDING_INTENT_URL)
+        val pendingIntentPendingIntent = PendingIntent.getActivity(context, PENDING_INTENT_PENDING_INTENT_ID, pendingIntentIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
         //To create a new notification, we specify both a context and the channel that was previously created
         val builder = NotificationCompat.Builder(applicationContext, MESSAGE_CHANNEL)
+
         //A notification at a minimum needs a small icon, title, and descriptive text.
         builder.apply {
             setSmallIcon(R.drawable.ic_launcher_foreground)
             setContentTitle("Random Int")
             setContentText("The random int generated is $data")
-            setContentIntent(browserPendingIntent)
+            setContentIntent(launchPendingIntent)
+            setAutoCancel(true)
+
+            //Each action has a title, icon, and pending intent
+            addAction(NotificationCompat.Action.Builder(
+                R.drawable.abc_list_divider_material,
+                context.getString(R.string.work_manager_action),
+                workManagerPendingIntent).build())
+
+            addAction(NotificationCompat.Action.Builder(
+                R.drawable.abc_cab_background_top_material,
+                context.getString(R.string.pending_intent_action),
+                pendingIntentPendingIntent
+            ).build())
         }
         val notificationManager = context.getSystemService(NotificationManager::class.java)
         //Each notification must have a unique ID as well
