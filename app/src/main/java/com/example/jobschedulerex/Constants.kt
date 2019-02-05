712e33f3c97db161c99d512bbec9b0c6fd6cf751
JGomez2542 <jason.gomez@mobileappscompany.com>
2019-02-05 12:04:31 -0500

    Added pendingIntent to handle notification taps

--- app/src/main/java/com/example/jobschedulerex/Constants.kt
+++ app/src/main/java/com/example/jobschedulerex/Constants.kt
@@ -5,3 +5,5 @@ const val BACKGROUND_RANDOM_TOAST_JOB_TAG = "random_toast"
 const val WORKER_RANDOM_TOAST = "worker_random_toast"
 const val MESSAGE_CHANNEL = "message_channel"
 const val MESSAGE_ID = 1
+const val PENDING_INTENT_ID = 2
+const val URL = "https://developer.android.com/topic/libraries/architecture/workmanager/"
--- app/src/main/java/com/example/jobschedulerex/NotificationWorker.kt
+++ app/src/main/java/com/example/jobschedulerex/NotificationWorker.kt
@@ -3,7 +3,10 @@ package com.example.jobschedulerex
 import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.support.v4.app.NotificationCompat
 import androidx.work.Worker
@@ -12,7 +15,8 @@ import androidx.work.WorkerParameters
 /**
  * This worker will be used to send a notification to the user.
  */
-class NotificationWorker(private val context: Context, workerParameters: WorkerParameters) : Worker(context, workerParameters) {
+class NotificationWorker(private val context: Context, workerParameters: WorkerParameters) :
+    Worker(context, workerParameters) {
 
     //Starting in Android 8.0 Oreo (API Level 26), all notifications must be associated with a notification channel
     //we create the channel here.
@@ -28,12 +32,19 @@ class NotificationWorker(private val context: Context, workerParameters: WorkerP
     @SuppressLint("NewApi")
     override fun doWork(): Result {
         val data = inputData.getString(WORKER_RANDOM_TOAST)
+        //Upon clicking the notification, we open up the browser.
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(URL)
+        val browserPendingIntent = PendingIntent.getActivity(context, PENDING_INTENT_ID, intent, PendingIntent.FLAG_UPDATE_CURRENT)
         //To create a new notification, we specify both a context and the channel that was previously created
         val builder = NotificationCompat.Builder(applicationContext, MESSAGE_CHANNEL)
         //A notification at a minimum needs a small icon, title, and descriptive text.
-        builder.setSmallIcon(R.drawable.ic_launcher_foreground)
-            .setContentTitle("Random Int")
-            .setContentText("The random int generated is $data")
+        builder.apply {
+            setSmallIcon(R.drawable.ic_launcher_foreground)
+            setContentTitle("Random Int")
+            setContentText("The random int generated is $data")
+            setContentIntent(browserPendingIntent)
+        }
         val notificationManager = context.getSystemService(NotificationManager::class.java)
         //Each notification must have a unique ID as well
         notificationManager.notify(MESSAGE_ID, builder.build())
